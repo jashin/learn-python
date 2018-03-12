02d2791ac62bfddc50ad307e2c679a43d1191911
Steven Zhan <steven.zhan@everbridge.com>
2018-03-11 23:44:34 -0400

    add commands to better understand the code

--- mytest/1 String/1.16_INterpolation_Variables.py
+++ mytest/1 String/1.16_INterpolation_Variables.py
@@ -5,9 +5,12 @@ def expand(format, d, marker='"', safe=False):
     def lookup(w): return d[w]
 
   parts = format.split(marker)
+  # This part of code expect the marker, in this case is double quote '"' is always
+  # used in a pair. So in that case, it can use stride 2 in the below code, it starts
+  # from the second element in the array, then travles the array using step 2.
   parts[1::2] = map(lookup, parts[1::2])
   return ''.join(parts)
 
 
 if __name__ == '__main__':
-  print expand('just "a" test test test"a""a" "a" "a" ', {'a': 'one'})
+  print expand('""just "a" test test test"a""a" "a" "a" ', {'a': 'one'}, safe=True)
